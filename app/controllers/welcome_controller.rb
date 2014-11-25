d6a66669a8fdc2af64452f540faf0b40bfef6f12
Brilliance Tech Sols <namanipradeepkumar@gmail.com>
2014-11-25 18:56:56 +0530

    30 pages completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,93 @@
 class WelcomeController < ApplicationController
   def index
+
   end
+
   def dashboard2
+  
+  end
+  def dashboard3
+  	
+  end
+  def graph_flot
+  	
+  end
+  def graph_morris
+  	
+  end
+  def graph_rickshaw
+  	
+  end
+  def graph_chartjs
+  	
+  end
+  def graph_peity
+  	
+  end
+  def graph_sparkline
+  	
+  end
+  def mailbox
+  	
+  end
+  def mail_detail
+  	
+  end
+  def mail_compose
+  	
+  end
+  def widgets
+  	
+  end
+  def form_basic
+  	
+  end
+  def form_advanced
+  	
+  end
+  def form_wizard
+  	
+  end
+  def form_file_upload
+  	
+  end
+  def form_editors
+  	
+  end
+  def contacts
+  	
+  end
+  def profile
+  	
+  end
+  def projects
+  	
+  end
+  def project_detail
+  	
+  end
+  def file_manager
+  	
+  end
+  def calendar
+  	
+  end
+  def faq
+  	
+  end
+  def timeline
+  	
+  end
+  def pin_board
+  	
+  end
+  def invoice
+  	
+  end
+  def login
+  	
+  end
+  def register
+  	
   end
 end
